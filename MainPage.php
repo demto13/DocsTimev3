82ece16a9e0fdb38f5d290c8bdcc65102ce01f0f
ToSzi <t.horvath@rgu.ac.uk>
2017-04-17 11:47:34 +0100

    Further improved Patient profile with actual data shown on screen

--- MainPage.php
+++ MainPage.php
@@ -85,11 +85,15 @@ __END;
                     }
 
 echo <<<__END
-                <form action="UploadFile.php" method="post" enctype="multipart/form-data">
-                    <input type="file" name="userPic">
-                    <button type="submit" name="submitPhoto">Submit photo!</button>
-                </form>
+                    <form action="UploadFile.php" method="post" enctype="multipart/form-data">
+                        <input type="file" name="userPic">
+                        <button type="submit" name="submitPhoto">Submit photo!</button>
+                    </form>
                 </div>
+__END;
+
+                if($_SESSION['type'] == "patient") {
+                    echo <<<__END
                 <div class="col-md-7 colTab">
                     <ul class="nav nav-tabs">
                         <li class="active"><a data-toggle="tab" href="#home">Home</a></li>
@@ -97,6 +101,91 @@ echo <<<__END
                         <li><a data-toggle="tab" href="#menu2">Search for doctor</a></li>
                         <li><a data-toggle="tab" href="#menu3">Book Appointment</a></li>
                         <li><a data-toggle="tab" href="#menu4">Manage appointments</a></li>
+                    </ul>
+                    <div class="tab-content">
+                        <div id="home" class="tab-pane fade in active">
+                            <h3>HOME</h3>
+                            <p>Welcome {$_SESSION['userName']} at DocsTime the ultimate doctors appointment booking system. Hope you will find it useful!</p>
+                            <p>There are doctors and patients registering every day so if you do not find what you are after, please check back later!</p>
+                        </div>
+                        <div id="menu1" class="tab-pane fade">
+                            <h3>My Profile</h3>
+                            <table class="table">
+                                <tr><th>Name</th><td>{$_SESSION['name']}</td></tr>
+                                <tr><th>Date of Birth</th><td>{$_SESSION['dob']}</td></tr>
+                                <tr><th>Email</th><td>{$_SESSION['email']}</td></tr>
+                                <tr><th>Phone</th><td>{$_SESSION['phone']}</td></tr>
+                                <tr><th>Username</th><td>{$_SESSION['userName']}</td></tr>
+                            </table>
+                            <button value="update" class="btn">Update</button>
+                        </div>
+                        <div id="menu2" class="tab-pane fade">
+                            <h3></h3>
+                            <form action="#" method="post">
+                                <div class="form-group">
+                                    <h1>Search our singles</h1>
+                                    <label for="gender">Gender</label>
+                                    <select id="gender" name="gender" class="form-control">
+                                        <option value="Female">Female</option>
+                                        <option value="Male">Male</option>
+                                    </select>
+                                    <label for="age">Age group</label>
+                                    <select id="age" class="form-control" name="age">
+                                        <option value="18-20">18-20</option>
+                                        <option value="21-25">21-25</option>
+                                        <option value="26-30">26-30</option>
+                                        <option value="30+">30+</option>
+                                    </select>
+                                    <label for="ethnicity">Ethnicity</label>
+                                    <select id="ethnicity" class="form-control" name="ethnicity">
+                                        <option value="white">White</option>
+                                        <option value="Black">Black</option>
+                                        <option value="Black">Asian</option>
+                                        <option value="Black">Indian</option>
+                                        <option value="Black">Other</option>
+                                    </select>
+                                </div>
+                                <input type="submit" class="btn" value="Search!">
+                            </form>
+                        </div>
+                        <div id="menu3" class="tab-pane fade">
+                            <h3>About me</h3>
+                                <h4>My interests and hobbies</h4>
+                                <p>I like sports and music and many many other things which you can find
+                                out if we can meet up for a drink or two!</p>
+                                <h4>I am looking for</h4>
+                                <p>A nice looking young person with a fun personality and
+                                great attitude. That person must be sporty and play music too.</p>
+                                <button value="update" class="btn">Update</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="row topics-box">
+                        <div class="topics">
+                            <h3>Quick Links</h3>
+                            <a href="http://www.bbc.co.uk/sport"><p>Sports</p></a>
+                            <a href="https://www.bbc.co.uk/music/news"><p>Music</p></a>
+                            <a href="http://www.imdb.com/"><p>Movies</p></a>
+                            <a href="http://www.rgu.ac.uk/news-and-events"><p>Campus news and events</p></a>
+                        </div>
+                    </div>
+                </div>
+            </div>  
+        </div>
+        
+__END;
+                }
+                else
+                {
+                    echo<<<__END
+                    <div class="col-md-7 colTab">
+                    <ul class="nav nav-tabs">
+                        <li class="active"><a data-toggle="tab" href="#home">Home</a></li>
+                        <li><a data-toggle="tab" href="#menu1">My Profile</a></li>
+                        <li><a data-toggle="tab" href="#menu2">Search for doctor</a></li>
+                        <li><a data-toggle="tab" href="#menu3">Manage Appointments</a></li>
+                        <li><a data-toggle="tab" href="#menu4">View Diary</a></li>
                     </ul>
                     <div class="tab-content">
                         <div id="home" class="tab-pane fade in active">
@@ -176,8 +265,9 @@ echo <<<__END
                 </div>
             </div>  
         </div>
-        
 __END;
+
+                }
 
 
     if(isset($_POST['logout']))
